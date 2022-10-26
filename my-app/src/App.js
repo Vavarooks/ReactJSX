0050729d336650d3781d8199fec8b65ca02ac2a4
Vanessa Emerick <vavarooks123@gmail.com>
2022-10-26 11:48:26 -0700

    React Dojo List

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hello Dojo
+      </h1>
+      <h3>Things I need to do:</h3>
+      <ls>
+        <ul>
+        Learn React
+        </ul>
+        <ul>
+        Climb Mt. Everest
+        </ul>
+        <ul>
+        Run a Marathon
+        </ul>
+        <ul>
+        Feed the dogs
+        </ul>
+      </ls>
     </div>
   );
 }
